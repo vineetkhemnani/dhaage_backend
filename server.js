04dc3ccaacaf6538b93850a65e457f124408a18e
vineetkhemnani <vineetkhemnani09@gmail.com>
2023-12-18 03:38:58 +0530

    added middlewares

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import connectDB from './db/connectDB.js'
-const app = express()
+import cookieParser from 'cookie-parser'
+
 dotenv.config()
+
 connectDB()
+const app = express()
 const PORT = process.env.PORT || 5000
+
+// middlewares
+app.use(express.json()) //parse JSON data in the req.body
+app.use(express.urlencoded({ extended: true })) //usually parses form data extended:true used to parse nested data if present
+app.use(cookieParser()) //parse cookies
+
+// Routes
+app.use('/api/users', userRoutes)
 
 app.listen(PORT, () => {
   console.log(`Server started at http://localhost:${PORT}`)
