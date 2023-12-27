0e8a4b7bfcf290898db0417ca80be2f376cb0af7
vineetkhemnani <vineetkhemnani09@gmail.com>
2023-12-27 20:47:26 +0530

    cannot follow/unfollow yourself bug fix

--- controllers/userController.js
+++ controllers/userController.js
@@ -97,7 +97,7 @@ export const followUnfollowUser = async (req, res) => {
     const userToModify = await User.findById(id)
     const currentUser = await User.findById(req.user._id)
 
-    if (id === req.user._id)
+    if (id === req.user._id.toString())
       return res
         .status(400)
         .json({ message: 'You cannot follow/unfollow yourself' })
