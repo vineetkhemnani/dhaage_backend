4cc68d3807781ffcd1df61c2c8d45a78ce444ac5
vineetkhemnani <vineetkhemnani09@gmail.com>
2024-01-01 23:00:41 +0530

    added cloudinary config and increased limit to backend requests

--- index.js
+++ index.js
@@ -5,15 +5,21 @@ import cookieParser from 'cookie-parser'
 import cors from 'cors'
 import userRoutes from './routes/userRoutes.js'
 import postRoutes from './routes/postRoutes.js'
-
+import { v2 as cloudinary } from 'cloudinary'
 dotenv.config()
 
 connectDB()
 const app = express()
 const PORT = process.env.PORT || 5000
 
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+})
+
 // middlewares
-app.use(express.json()) //parse JSON data in the req.body
+app.use(express.json({ limit: '50mb' })) //parse JSON data in the req.body
 app.use(express.urlencoded({ extended: true })) //usually parses form data extended:true used to parse nested data if present
 app.use(cookieParser()) //parse cookies
 var allowedOrigins = [
@@ -26,7 +32,7 @@ var allowedOrigins = [
 app.use(
   cors({
     origin: allowedOrigins,
-    methods:["POST","GET","DELETE","PUT"],
+    methods: ['POST', 'GET', 'DELETE', 'PUT'],
 
     // exposedHeaders: ['Content-Length', 'X-Foo', 'X-Bar'],
 
@@ -37,8 +43,8 @@ app.use(
 // Routes
 app.use('/api/users', userRoutes)
 app.use('/api/posts', postRoutes)
-app.use('/',(req,res)=>{
-  res.send("Hello")
+app.use('/', (req, res) => {
+  res.send('Hello')
 })
 app.listen(PORT, () => {
   console.log(`Server started at http://localhost:${PORT}`)
