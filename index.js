a4e1177ceff86470da0bfab22d927b9e2d944906
vineetkhemnani <vineetkhemnani09@gmail.com>
2024-01-01 18:24:41 +0530

    added cors to backend

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from 'express'
 import dotenv from 'dotenv'
 import connectDB from './db/connectDB.js'
 import cookieParser from 'cookie-parser'
-
+import cors from 'cors'
 import userRoutes from './routes/userRoutes.js'
 import postRoutes from './routes/postRoutes.js'
 
@@ -16,6 +16,23 @@ const PORT = process.env.PORT || 5000
 app.use(express.json()) //parse JSON data in the req.body
 app.use(express.urlencoded({ extended: true })) //usually parses form data extended:true used to parse nested data if present
 app.use(cookieParser()) //parse cookies
+var allowedOrigins = [
+  'https://dhaage-backend.vercel.app',
+  'https://dhaage.vercel.app',
+  'https://dhaage.netlify.app',
+  'http://localhost:3000',
+]
+
+app.use(
+  cors({
+    origin: allowedOrigins,
+    methods:["POST","GET","DELETE","PUT"],
+
+    // exposedHeaders: ['Content-Length', 'X-Foo', 'X-Bar'],
+
+    credentials: true,
+  })
+)
 
 // Routes
 app.use('/api/users', userRoutes)
