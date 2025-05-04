37d61dc69804555b6d00ae01f74096a0ec66e691
vineetkhemnani <vineetkhemnani09@gmail.com>
2025-05-04 14:05:52 +0530

    Update Google OAuth callback URL to use environment variable for production

--- utils/config/passport.js
+++ utils/config/passport.js
@@ -12,7 +12,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: 'http://localhost:8000/api/users/auth/google/callback',
+      callbackURL: `${process.env.BACKEND_URL_PROD}/api/users/auth/google/callback`,
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
