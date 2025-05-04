9712df9f1bb12eb2b61bcf2f5ae59c69cc521811
vineetkhemnani <vineetkhemnani09@gmail.com>
2025-05-04 12:36:14 +0530

    Fix typo in handleGoogleLogin function: corrected 'setCoofkie' to 'setCookie'

--- controllers/userController.js
+++ controllers/userController.js
@@ -90,7 +90,7 @@ export const handleGoogleLogin = async (req, res) => {
     if (!user) {
       return res.status(400).json({ error: 'Google authentication failed' });
     }
-    generateTokenAndSetCookie(user._id, res);
+    generateTokenAndSetCoofkie(user._id, res);
     res.status(200).json({
       _id: user._id,
       name: user.name,
